7e5c4c4773409a7a219f54b52d5f728b5b688a03
Eugene <eugene132003@list.ru>
2023-04-27 14:51:35 +0300

    start codding

--- GB_Kontrol_01.cs
+++ GB_Kontrol_01.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Слова, числа или символы, разделяя их \", \": ");
+int stringInner = Console.ReadLine();
+String[] stringNew = stringInner.Split(", ");
+CutStringToArray(stringNew);
+
+String[] CutStringToArray(String[] stroka)
+{
+    int[] array = new int[];
+Console.Write($"[{String.Join(", ", stroka)}]");
+    return array;
+}
+
+void PrintArray(int[] inArray)
+{
+    Console.Write($"[{String.Join(", ", inArray)}]");
+}
